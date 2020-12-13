e568135d81d1684ff43d54d34b83061b3f44e990
lukasz <lukaszmac13@gmail.com>
2020-12-14 00:35:47 +0100

    NumberUtils test balance overdraft

--- src/app/shared/utils/number-utils.spec.ts
+++ src/app/shared/utils/number-utils.spec.ts
@@ -1,14 +1,21 @@
 import { TestSource } from "jasmine-data-provider-ts";
 import { NumberUtils } from "./number-utils";
 
-interface TestCase {
+interface TwoDecimalTestCase {
   value: number;
   roundValue: number;
 }
 
-describe('NumberUtils', () => {
+interface OverdraftTestCase {
+  amount: number;
+  balance: number;
+  maxOverdraft: number;
+  isOverdraft: boolean;
+}
 
-  function provider(): TestCase[] {
+describe('NumberUtils test 2 decimal', () => {
+
+  function provider(): TwoDecimalTestCase[] {
     return [
       { value: 0.055, roundValue: 0.06 },
       { value: 0.054, roundValue: 0.05 },
@@ -21,9 +28,32 @@ describe('NumberUtils', () => {
     ];
   }
 
-  TestSource<TestCase>(provider, (data) => {
-    it(`should round 2 decimal ${data.value} to ${data.roundValue}`, () => {
+  TestSource<TwoDecimalTestCase>(provider, (data) => {
+    it(`should round ${data.value} to ${data.roundValue}`, () => {
       expect(NumberUtils.round2(data.value)).toEqual(data.roundValue);
+    });
+  });
+});
+
+describe('NumberUtils test balance overdraft', () => {
+
+  function provider(): OverdraftTestCase[] {
+    return [
+      { amount: 1, balance: 1000, maxOverdraft: -500, isOverdraft: false },
+      { amount: 1000, balance: 1000, maxOverdraft: -500, isOverdraft: false },
+      { amount: 1400, balance: 1000, maxOverdraft: -500, isOverdraft: false },
+      { amount: 1500, balance: 1000, maxOverdraft: -500, isOverdraft: false },
+      { amount: 1501, balance: 1000, maxOverdraft: -500, isOverdraft: true },
+      { amount: 1500.01, balance: 1000, maxOverdraft: -500, isOverdraft: true },
+      { amount: 700, balance: 1000, maxOverdraft: 200, isOverdraft: false },
+      { amount: 800, balance: 1000, maxOverdraft: 200, isOverdraft: false },
+      { amount: 900, balance: 1000, maxOverdraft: 200, isOverdraft: true },
+    ];
+  }
+
+  TestSource<OverdraftTestCase>(provider, (data) => {
+    it(`should return ${data.isOverdraft} when tranferAmount = ${data.amount}, balance = ${data.balance}, maxOverdraft = ${data.maxOverdraft}`, () => {
+      expect(NumberUtils.balanceOverdraft(data.amount, data.balance, data.maxOverdraft)).toBe(data.isOverdraft);
     });
   });
 });
